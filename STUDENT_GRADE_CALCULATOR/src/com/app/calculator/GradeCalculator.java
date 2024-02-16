af3bc966daba8d11974b765e479b7a8db366ab42
TarjanSahu <tarjansahu6@gmail.com>
2024-02-16 23:49:02 +0530

    add all the code in Tester

--- STUDENT_GRADE_CALCULATOR/src/com/app/calculator/GradeCalculator.java
+++ STUDENT_GRADE_CALCULATOR/src/com/app/calculator/GradeCalculator.java
@@ -1,5 +1,33 @@
 package com.app.calculator;
 
-public class GradeCalculator {
+import java.util.List;
 
+public class GradeCalculator {
+	
+    public static int calculateTotalMarks(List<Integer> marksList) {
+        int totalMarks = 0;
+        for (int marks : marksList) {
+            totalMarks += marks;
+        }
+        return totalMarks;
+    }
+    
+    public static double calculateAveragePercentage(int totalMarks, int numSubjects) {
+        return (double) totalMarks / numSubjects;
+    }
+    
+    public static char calculateGrade(double percentage) {
+        if (percentage >= 90) {
+            return 'A';
+        } else if (percentage >= 80) {
+            return 'B';
+        } else if (percentage >= 70) {
+            return 'C';
+        } else if (percentage >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
 }
+
--- STUDENT_GRADE_CALCULATOR/src/tester/Tester.java
+++ STUDENT_GRADE_CALCULATOR/src/tester/Tester.java
@@ -1,10 +1,54 @@
 package tester;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.app.calculator.GradeCalculator;
+
 public class Tester {
+    public static void main(String[] args) {
+        List<Integer> marksList = new ArrayList<>();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("Choose an option:");
+                System.out.println("1. Enter marks for a subject");
+                System.out.println("2. Calculate total marks and average percentage");
+                System.out.println("3. Calculate grade");
+                System.out.println("4. Exit");
 
-	}
-
+                int option = scanner.nextInt();
+                switch (option) {
+                    case 1:
+                        System.out.print("Enter marks obtained in the subject: ");
+                        int marks = scanner.nextInt();
+                        if(marks <= 100) {
+                        	marksList.add(marks);
+                        }else {
+                        	System.out.println("Number should be less then or equel to  100");
+                        }
+                        
+                        break;
+                    case 2:
+                        int totalMarks = GradeCalculator.calculateTotalMarks(marksList);
+                        double averagePercentage = GradeCalculator.calculateAveragePercentage(totalMarks, marksList.size());
+                        System.out.println("Total Marks: " + totalMarks);
+                        System.out.println("Average Percentage: " + averagePercentage + "%");
+                        break;
+                    case 3:
+                        double avgPercentage = GradeCalculator.calculateAveragePercentage(GradeCalculator.calculateTotalMarks(marksList), marksList.size());
+                        char grade = GradeCalculator.calculateGrade(avgPercentage);
+                        System.out.println("Grade: " + grade);
+                        break;
+                    case 4:
+                        return;
+                    default:
+                        System.out.println("Invalid option. Please choose again.");
+                }
+            }
+        }catch (Exception e) {
+        	e.printStackTrace();
+        }
+    }
 }
